408c8c53db74aaba5831d42ad710aff216c7b2ba
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2020-06-20 01:25:58 +0530

    Add files via upload

---
+++ Random-enemy-strike/Resources/meta_tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="meta_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <properties>
+  <property name="Collidable" value="True"/>
+ </properties>
+ <image source="meta_tiles.png" width="67" height="34"/>
+</tileset>
